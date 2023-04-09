aaddb425882f9f65509059de50ca621d1b9e6870
ubcom245 <yenkakakaka@gmail.com>
2023-04-09 10:08:26 +0700

    update

--- main.py
+++ main.py
@@ -253,6 +253,11 @@ def main(args):
                 del checkpoint_model[k]
         interpolate_pos_embed(model_without_ddp, checkpoint_model)
         model_without_ddp.load_state_dict(checkpoint['model'], strict=False)
+        if args.poor:
+            for _, block in enumerate(model.backbone.backbone.blocks):
+                if _ < 8:
+                    for param in block.parameters():
+                        param.requires_grad = False
         # unexpected_keys = [k for k in unexpected_keys if not (k.endswith('total_params') or k.endswith('total_ops'))]
         # if len(missing_keys) > 0:
         #     print('Missing Keys: {}'.format(missing_keys))
