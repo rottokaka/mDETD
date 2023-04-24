0c8ee5bd953f906885627988e3b88e02581b9a05
ubcom245 <yenkakakaka@gmail.com>
2023-04-24 17:05:15 +0700

    update

--- app.py
+++ app.py
@@ -1,6 +1,80 @@
 from flask import Flask, render_template, request
+import os
+import time
+import argparse
+
+import torch
+
+from main import get_args_parser
+from models import build_model
+import torch
+from torch import nn
+import torchvision.transforms as T
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+# COCO classes
+CLASSES = [
+    'N/A', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A',
+    'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack',
+    'umbrella', 'N/A', 'N/A', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis',
+    'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+    'skateboard', 'surfboard', 'tennis racket', 'bottle', 'N/A', 'wine glass',
+    'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich',
+    'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake',
+    'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table', 'N/A',
+    'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard',
+    'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A',
+    'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+    'toothbrush'
+]
+
+# colors for visualization
+COLORS = [[0.000, 0.447, 0.741], [0.850, 0.325, 0.098], [0.929, 0.694, 0.125],
+          [0.494, 0.184, 0.556], [0.466, 0.674, 0.188], [0.301, 0.745, 0.933]]
+
+# standard PyTorch mean-std input image normalization
+transform = T.Compose([
+    T.Resize(800),
+    T.ToTensor(),
+    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# for output bounding box post-processing
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h),
+         (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+def get_results(pil_img, prob, boxes):
+    colors = COLORS * 100
+    for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+        cl = p.argmax()
+        text = f'{CLASSES[cl]}: {p[cl]:0.2f}'
+    return text
+
+parser = argparse.ArgumentParser('ViT Deformable DETR training and evaluation script', parents=[get_args_parser("debug", "0")])
+args = parser.parse_args()
 
 app = Flask(__name__)
+model = None
+if model is None:
+    model, _, _ = build_model(args)
+    if args.resume is not None:
+        ckpt = torch.load(args.resume, map_location=lambda storage, loc: storage)
+        model.load_state_dict(ckpt['model'])
+    model.cuda()
+model.eval()
 
 @app.route('/', methods=['GET'])
 def hello_word():
@@ -9,10 +83,27 @@ def hello_word():
 @app.route('/', methods=['POST'])
 def predict():
     img_file = request.files['img_file']
-    img_path = "./images/"+img_file.filename
+    img_path = "./mDETD/images/"+img_file.filename
     img_file.save(img_path)
 
-    return render_template('index.html')
+    # version = request.form.get('version')
+    im = Image.open(img_path)
+
+    # mean-std normalize the input image (batch-size: 1)
+    img = transform(im).unsqueeze(0)
+
+    # propagate through the model
+    # outputs = model([img.squeeze().to('cuda:0')])
+
+    # # keep only predictions with 0.7+ confidence
+    # probas = outputs['pred_logits'].softmax(-1)[0, :, :-1]
+    # keep = probas.max(-1).values >= min(torch.topk(probas.max(-1).values,1).values).item()
+
+    # # convert boxes from [0; 1] to image scales
+    # bboxes_scaled = rescale_bboxes(outputs['pred_boxes'][0, keep].to('cpu'), im.size)
+
+    # text = get_results(im, probas[keep], bboxes_scaled)
+    return render_template('index.html', prediction="lol")
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
